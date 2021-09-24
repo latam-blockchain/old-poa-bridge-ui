d52ef40d4de5191dd960a2f4159114435c4e8163
diego20lik <44272488+diegoanvilla@users.noreply.github.com>
2021-09-24 12:14:27 -0400

    Redesign

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,15 +1,16 @@
-import React from "react"
-import { IconGithub, IconPOA, IconTelegram, IconTwitter } from "./social-icons"
+import React from "react";
+import { IconGithub, IconPOA, IconTelegram, IconTwitter } from "./social-icons";
+import Logo from "../assets/images/themes/core/logos/logo_big.png";
 
 export const SocialIcons = () => {
   const socialItems = [
     {
-      icon: <IconPOA />,
-      link: "https://poa.network",
+      icon: <img src={Logo} width="70%"></img>,
+      link: "https://www.latam-blockchain.com/",
     },
     {
       icon: <IconTwitter />,
-      link: "https://twitter.com/poanetwork",
+      link: "https://twitter.com/LatamOnChain",
     },
     {
       icon: <IconTelegram />,
@@ -19,17 +20,22 @@ export const SocialIcons = () => {
       icon: <IconGithub />,
       link: "https://github.com/poanetwork/token-bridge",
     },
-  ]
+  ];
 
   return (
     <div className="social-icons">
       {socialItems.map((item, index) => {
         return (
-          <a key={index} href={item.link} target="_blank" className="social-icons-item">
+          <a
+            key={index}
+            href={item.link}
+            target="_blank"
+            className="social-icons-item"
+          >
             {item.icon}
           </a>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
