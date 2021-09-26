0459b49b9a4775a6a9ae9551a4e73cd580b5ffad
mrwh0 <betthis45@gmail.com>
2021-09-26 10:05:58 -0400

    Updates

---
+++ precache-manifest.c9ad8d59d6ded24354c08f3cee1d28fa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "71e27905d41f0bcc847920ae86ffce7e",
+    "url": "/old-poa-bridge-ui/static/media/close.71e27905.svg"
+  },
+  {
+    "revision": "56fa0a1a606755ca6255",
+    "url": "/old-poa-bridge-ui/static/css/main.75cea353.chunk.css"
+  },
+  {
+    "revision": "cc0bb5dc83561017a831",
+    "url": "/old-poa-bridge-ui/static/js/runtime~main.7823d358.js"
+  },
+  {
+    "revision": "679cdcc2053b8e4d4f09",
+    "url": "/old-poa-bridge-ui/static/js/2.c1053822.chunk.js"
+  },
+  {
+    "revision": "479d6f1cae07775abffd7db437ba4c2c",
+    "url": "/old-poa-bridge-ui/static/media/icon-arrows@2x.479d6f1c.png"
+  },
+  {
+    "revision": "7946b8a285034a488e7797656efdccde",
+    "url": "/old-poa-bridge-ui/static/media/logo_big.7946b8a2.png"
+  },
+  {
+    "revision": "99716024dfc52f240afa4558b56a4545",
+    "url": "/old-poa-bridge-ui/static/media/disclaimer@2x.99716024.png"
+  },
+  {
+    "revision": "676164754ba93697d6be7adb2b4af38f",
+    "url": "/old-poa-bridge-ui/static/media/i@3x.67616475.png"
+  },
+  {
+    "revision": "d7d7c4c6dbe228b50b6349e6e0fdf4b8",
+    "url": "/old-poa-bridge-ui/static/media/center-image-pointing-right.d7d7c4c6.svg"
+  },
+  {
+    "revision": "56fa0a1a606755ca6255",
+    "url": "/old-poa-bridge-ui/static/js/main.ab5fdb15.chunk.js"
+  },
+  {
+    "revision": "0a8547da65ba0c353ac675ec5bbf9dd8",
+    "url": "/old-poa-bridge-ui/static/media/center-image-pointing-left.0a8547da.svg"
+  },
+  {
+    "revision": "4c8e412f855fd76e3fc4058e8027e7d9",
+    "url": "/old-poa-bridge-ui/static/media/logo-loading.4c8e412f.svg"
+  },
+  {
+    "revision": "2f0d9a00f867d98726e37f86e3a594a9",
+    "url": "/old-poa-bridge-ui/static/media/logo-0.2f0d9a00.svg"
+  },
+  {
+    "revision": "159133e3487fe0814552e003dc442df7",
+    "url": "/old-poa-bridge-ui/static/media/logo-2.159133e3.svg"
+  },
+  {
+    "revision": "84e0b0dc8fc38785fffd33babe7ec4f3",
+    "url": "/old-poa-bridge-ui/static/media/logo-1.84e0b0dc.svg"
+  },
+  {
+    "revision": "f5f4685c4a5eeea9f0df5e7bfc1d0823",
+    "url": "/old-poa-bridge-ui/static/media/logo.f5f4685c.png"
+  },
+  {
+    "revision": "fc35dd805bee198dedfe7ade1a78869b",
+    "url": "/old-poa-bridge-ui/static/media/logo_big-white.fc35dd80.png"
+  },
+  {
+    "revision": "c7edef1fc4f3e0d0f7b9d913e0517f17",
+    "url": "/old-poa-bridge-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.735df688807a7fea3d3c0d505b605c02.js"
+  "/old-poa-bridge-ui/precache-manifest.c9ad8d59d6ded24354c08f3cee1d28fa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/old-poa-bridge-ui/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
