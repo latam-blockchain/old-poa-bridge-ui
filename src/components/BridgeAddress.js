3992260575f1a1636d83d6a8d6b286b9b0722cd6
diego20lik <44272488+diegoanvilla@users.noreply.github.com>
2021-11-26 10:55:56 -0400

    Adding of foreign logo section in bridge

--- src/components/BridgeAddress.js
+++ src/components/BridgeAddress.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export const BridgeAddress = ({ isHome, reverse }) => {
-  const getAddress = () => isHome ?
-    (<div className="home-address-container" />)
-    :
-    (<div className="foreign-address-container" />)
+  const getAddress = () =>
+    isHome ? (
+      <div className="home-address-container" />
+    ) : (
+      <div className="foreign-address-container" />
+    );
 
-  return isHome ?
-    (<div className="bridge-home">
+  return isHome ? (
+    <div className="bridge-home">
       <div className="bridge-home-container">
         <div className="home-logo-container">
-          <div className={reverse ? 'foreign-logo' : 'home-logo'} />
+          {reverse ? (
+            <div>
+              <h3>Rinkeby</h3>
+            </div>
+          ) : (
+            <div className="home-logo"></div>
+          )}
+          {/* <div className={reverse ? "foreign-logo" : "home-logo"} /> */}
         </div>
       </div>
       {getAddress()}
-    </div>)
-    :
-    (<div className="bridge-foreign">
+    </div>
+  ) : (
+    <div className="bridge-foreign">
       {getAddress()}
       <div className="bridge-foreign-container">
         <div className="foreign-logo-container">
-          <div className={reverse ? 'home-logo' : 'foreign-logo'} />
+          {reverse ? (
+            <div className="home-logo"></div>
+          ) : (
+            <div>
+              <h3>Rinkeby</h3>
+            </div>
+          )}
+          {/* <div className={reverse ? "home-logo" : "foreign-logo"} /> */}
         </div>
       </div>
-    </div>)
-}
+    </div>
+  );
+};
